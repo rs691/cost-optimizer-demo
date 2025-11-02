464f3c315eabca59ab71371df854ea43a982da70
rs691 <robstewart@my365.bellevue.edu>
2025-11-02 11:43:06 -0600

    change yaml file

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'cost-optimizer-demo/',
+  base: '/',
 })
